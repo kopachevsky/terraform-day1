bc82539aba14661d256d282eae41ba173ab32d3f
antnknvlnk <akonovalenko@crmdata.info>
2024-04-01 10:58:24 +0300

    Removed comment

--- main.tf
+++ main.tf
@@ -32,5 +32,3 @@ resource "local_file" "index_html" {
   content  = templatefile("${path.module}/templates/index.tpl", { message = var.message })
   filename = "${path.module}/content/index.html"
 }
-
-# Видаліть або закоментуйте дублюючий ресурс "docker_container" "nginx", щоб уникнути помилки.
